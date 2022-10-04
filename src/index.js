e03fa7697804b77df8ebbb455a103e7af0ef766c
sstaar91 <sstaar91@gmail.com>
2022-10-04 22:07:42 +0900

    ADD:메인 페이지 UI 구현

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Login from './Login';
+import Login from './pages/Login/Login';
+import Main from './pages/Main/Main';
+import './styles/reset.css';
+import './styles/common.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Login />);
+root.render(<Main />);
