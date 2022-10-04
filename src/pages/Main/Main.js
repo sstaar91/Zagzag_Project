794d1f11ff2ba9f565fa13dc8b0b2a2f8dfef626
sstaar91 <sstaar91@gmail.com>
2022-10-04 22:07:42 +0900

    ADD:메인 페이지 UI 구현

---
+++ src/pages/Main/Main.js
@@ -0,0 +1,29 @@
+import { faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './Main.css';
+
+const Main = () => {
+  return (
+    <nav>
+      <div className="header">
+        <span className="logo">Zagzag</span>
+        <div className="iconWrap">
+          <FontAwesomeIcon className="icon" icon={faCartShopping} size="xl" />
+          <FontAwesomeIcon className="icon" icon={faUser} size="xl" />
+        </div>
+      </div>
+      <ul className="menu">
+        <li>홈</li>
+        <li>매거진</li>
+        <li>단독</li>
+        <li>브랜드</li>
+      </ul>
+      <div className="main">
+        <img className="mainImage" src="./images/main.jpg" alt="main" />
+        <div className="mainTitle"> [ OOTD : 주차룩 ] </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Main;
