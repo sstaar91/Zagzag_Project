13ffa80952f6acd0a4b6c21d72dc5f7de943efb1
sstaar91 <sstaar91@gmail.com>
2022-10-10 02:40:54 +0900

    ADD : setting

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,5 +1,9 @@
-import { faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCartShopping,
+  faSearch,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 import './Main.scss';
 
 const Main = () => {
@@ -7,6 +11,10 @@ const Main = () => {
     <nav className="main">
       <div className="header">
         <span className="logo">Zagzag</span>
+        <div className="searchBox">
+          <input className="searchInput" placeholder="검색" />
+          <FontAwesomeIcon className="searchIcon" icon={faSearch} />
+        </div>
         <div className="iconWrap">
           <FontAwesomeIcon className="icon" icon={faCartShopping} size="xl" />
           <FontAwesomeIcon className="icon" icon={faUser} size="xl" />
@@ -14,13 +22,13 @@ const Main = () => {
       </div>
       <ul className="menu">
         <li>홈</li>
-        <li>매거진</li>
-        <li>단독</li>
-        <li>브랜드</li>
+        <li>기획전</li>
+        <li>쇼핑몰</li>
+        <li>룩북</li>
       </ul>
-      <div className="main">
+      <div className="feed">
         <img className="mainImage" src="./images/main.jpg" alt="main" />
-        <div className="mainTitle"> [ OOTD : 주차룩 ] </div>
+        <div className="mainTitle"> [ 오늘의 룩 : 주차룩 ] </div>
       </div>
     </nav>
   );
