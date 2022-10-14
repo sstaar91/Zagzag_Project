9574e94445b980bb5631781a0c3dd91d9671dfe3
sstaar91 <sstaar91@gmail.com>
2022-10-14 22:45:32 +0900

    ADD : Login 기능 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,31 @@
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 const Login = () => {
+  const [userInfo, setUserInfo] = useState({
+    id: '',
+    pw: '',
+  });
+  const { id, pw } = userInfo;
   const navigate = useNavigate();
-  const clickBtn = () => {
-    navigate('/main');
+
+  const handleUserInfo = e => {
+    const { name, value } = e.target;
+    setUserInfo({ ...userInfo, [name]: value });
   };
+
+  const clickBtn = () => {
+    if (id === trueId && pw === truePassword) {
+      alert('로그인 성공');
+      navigate('/main');
+    } else {
+      alert('다시하세요');
+    }
+  };
+
+  const trueId = 'bluemind';
+  const truePassword = '1234asdf';
 
   return (
     <div className="login">
@@ -17,6 +37,8 @@ const Login = () => {
             className="input"
             type="text"
             placeholder="아이디를 입력하세요"
+            name="id"
+            onChange={handleUserInfo}
           />
         </div>
         <div className="inputBox">
@@ -24,7 +46,9 @@ const Login = () => {
           <input
             className="input"
             type="password"
+            name="pw"
             placeholder="비밀번호를 입력하세요"
+            onChange={handleUserInfo}
           />
         </div>
         <button className="loginBtn" onClick={clickBtn}>
